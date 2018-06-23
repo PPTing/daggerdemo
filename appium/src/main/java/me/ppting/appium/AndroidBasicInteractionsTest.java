bdb787ab1a6c9ecead0681ae8e76918d5ab0f23b
郑克建 <zhengkejian0@gmail.com>
2018-06-23 09:33:39 +0800

    添加 appium

---
+++ appium/src/main/java/me/ppting/appium/AndroidBasicInteractionsTest.java
@@ -0,0 +1,75 @@
+package me.ppting.appium;
+
+/**
+ * Created by PPTing on 2018/6/22.
+ * Description:
+ */
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class AndroidBasicInteractionsTest extends BaseTest {
+    private AndroidDriver<WebElement> driver;
+    private final String SEARCH_ACTIVITY = ".app.SearchInvoke";
+    private final String ALERT_DIALOG_ACTIVITY = ".app.AlertDialogSamples";
+    private final String PACKAGE = "io.appium.android.apis";
+
+    @BeforeClass
+    public void setUp() throws IOException {
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "../apps");
+        File app = new File(appDir.getCanonicalPath(), "ApiDemos-debug.apk");
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setPlatform(org.openqa.selenium.Platform.ANDROID);
+        driver = new AndroidDriver<WebElement>(getServiceUrl(), new DesiredCapabilities());
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+
+    @Test()
+    public void testSendKeys() {
+//        driver.startActivity(new Activity(PACKAGE, SEARCH_ACTIVITY));
+//        AndroidElement searchBoxEl = (AndroidElement) driver.findElementById("txt_query_prefill");
+//        searchBoxEl.sendKeys("Hello world!");
+//        AndroidElement onSearchRequestedBtn = (AndroidElement) driver.findElementById("btn_start_search");
+//        onSearchRequestedBtn.click();
+//        AndroidElement searchText = (AndroidElement) new WebDriverWait(driver, 30)
+//                .until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/search_src_text")));
+//        String searchTextValue = searchText.getText();
+//        Assert.assertEquals(searchTextValue, "Hello world!");
+    }
+
+    @Test
+    public void testOpensAlert() {
+        // Open the "Alert Dialog" activity of the android app
+        driver.startActivity(new Activity(PACKAGE, ALERT_DIALOG_ACTIVITY));
+
+        // Click button that opens a dialog
+        AndroidElement openDialogButton = (AndroidElement) driver.findElementById("io.appium.android.apis:id/two_buttons");
+        openDialogButton.click();
+
+        // Check that the dialog is there
+        AndroidElement alertElement = (AndroidElement) driver.findElementById("android:id/alertTitle");
+        String alertText = alertElement.getText();
+        Assert.assertEquals(alertText, "Lorem ipsum dolor sit aie consectetur adipiscing\nPlloaso mako nuto siwuf cakso dodtos anr koop.");
+        AndroidElement closeDialogButton = (AndroidElement) driver.findElementById("android:id/button1");
+
+        // Close the dialog
+        closeDialogButton.click();
+    }
+}
